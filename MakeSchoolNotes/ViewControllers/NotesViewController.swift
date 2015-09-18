f4ac67831223c4a05aab8cfd239fbdf5a5884859
Ben-G <Benjamin.Encz@gmail.com>
2015-09-18 06:00:08 +0200

    Fix ConvenienceKit Linking

--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -13,6 +13,7 @@ class NotesViewController: UITableViewController {
  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
   
